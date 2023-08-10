9231fa1e9f66a9ddeec498b3e0ebcb3a196ddef8
alex <alexander.plaut@gmail.com>
2023-08-10 11:21:56 -0400

    added page links

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Contacts from './pages/contacts/Contacts'
+import Gallery from './pages/gallery/Gallery'
+import Plans from './pages/plans/Plans'
+import Trainers from './pages/trainers/Trainers'
+import NotFound from './pages/notFound/NotFound'
+
 
 const App = () => {
   return (
-    <div>React Website Butcho</div>
+    <div>
+      <Home/>
+      <About/>
+      <Contacts/>
+      <Gallery/>
+      <Plans/>
+      <Trainers/>
+      <NotFound/>
+    </div>
   )
 }
 
