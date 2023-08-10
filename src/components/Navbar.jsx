5e185c955fcf7c118b94e23667a0a194c78364ac
alex <alexander.plaut@gmail.com>
2023-08-10 11:38:57 -0400

    added navbar components and js objects, array

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div className="container nav_container">
+            logo
+            nav links
+            button
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
