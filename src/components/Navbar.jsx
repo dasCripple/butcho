3a8c297b30c36947f70d31329543766408ee0cbf
alex <alexander.plaut@gmail.com>
2023-08-10 16:27:46 -0400

    import linksfrom data

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import {Link, NavLink} from 'react-router-dom'
-
-import './navbar.css'
 import Logo from '../images/logo.png'
+import {links} from '../data'
+import './navbar.css'
 
 const Navbar = () => {
   return (
