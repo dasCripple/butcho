e22162723fec9dbc0bb0566a9d1351487e20b2d0
alex <alexander.plaut@gmail.com>
2023-08-23 13:42:07 -0400

    error update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,36 @@
 import {Link, NavLink} from 'react-router-dom'
 import Logo from '../images/logo.png'
 import {links} from '../data'
-import {FaBars} from 'react-icons/go'
+import {GoThreeBars} from 'react-icons/go'
 import  './navbar.css'
 
 const Navbar = () => {
   return (
     <nav>
-        <div className="container nav_container">
-            <Link to="/" className='logo'>
-              <img src={Logo} alt="Nav Logo" />
-            </Link>
-            <ul className='nav_links'>
-                {
-                  links.map(({name, path}, index) => {
-                    return (
-                        <li>
-                          <NavLink to={path}>{name}</NavLink>
-                        </li>
-                    )
-                  })
-                }
-            </ul>
-            <button className="nav_toggle-btn"></button>
-        </div>
+      <div className="container nav_container">
+        <Link to="/" className='logo'>
+          <img src={Logo} alt="Nav Logo" />
+        </Link>
+        <ul className='nav_links'>
+          {
+            links.map(({name, path}, index) => {
+              return (
+                <li>
+                    <NavLink to={path}>{name}</NavLink>
+                </li>
+              )
+            })
+          }
+        </ul>
+        <button className="nav_toggle-btn">
+          <GoThreeBars></GoThreeBars>
+        </button>
+      </div>        
     </nav>
   )
-}
+        }
+
+
 
 export default Navbar
+
